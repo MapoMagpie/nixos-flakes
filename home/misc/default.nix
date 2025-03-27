ed470a4aba7f8b73cc5fb56bef37b96c1ad3bd33
MapoMagpie <zsyjk@live.cn>
2025-03-27 12:28:49 +0800

    add: local fonts;

--- home/misc/default.nix
+++ home/misc/default.nix
@@ -1,4 +1,8 @@
 { config, ... }:
 {
+  imports = [
+    ./fonts
+  ];
+
   home.file.".config/swayimg/config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/home/misc/swayimg_config.ini";
 }
