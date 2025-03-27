6d2e10ec0433bcab91ceb3fb857a00caf287a99e
MapoMagpie <zsyjk@live.cn>
2025-03-27 12:31:25 +0800

    change: fonts;

--- home/misc/default.nix
+++ home/misc/default.nix
@@ -1,8 +1,6 @@
 { config, ... }:
 {
-  imports = [
-    ./fonts
-  ];
-
+  home.file.".local/share/fonts".source = ./fonts;
+  fonts.fontconfig.enable = true;
   home.file.".config/swayimg/config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/home/misc/swayimg_config.ini";
 }
