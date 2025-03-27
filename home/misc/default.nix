57589c32d9eb3792ffa0467bbad55fa55bb40227
MapoMagpie <zsyjk@live.cn>
2025-03-27 16:03:47 +0800

    update: fonts;

--- home/misc/default.nix
+++ home/misc/default.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  home.file.".local/share/fonts".source = ./fonts;
+  home.file.".local/share/fonts".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/home/misc/fonts";
   fonts.fontconfig.enable = true;
   home.file.".config/swayimg/config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/home/misc/swayimg_config.ini";
 }
