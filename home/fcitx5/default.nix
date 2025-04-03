88310da3cea5601adfda2d6da7336e093c2ecd6f
MapoMagpie <zsyjk@live.cn>
2025-04-04 00:24:58 +0800

    change: fcitx5;

--- home/fcitx5/default.nix
+++ home/fcitx5/default.nix
@@ -1,6 +1,21 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   home.file.".local/share/fcitx5/rime" = {
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/home/fcitx5/rime";
+  };
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-rime
+        fcitx5-gtk
+        fcitx5-nord
+        fcitx5-configtool
+        fcitx5-catppuccin
+        fcitx5-tokyonight
+        libsForQt5.fcitx5-qt
+      ];
+    };
   };
 }
