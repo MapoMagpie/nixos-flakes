98dc1712ad49999d52876b5746a215e320577d3c
MapoMagpie <zsyjk@live.cn>
2025-05-05 01:35:11 +0800

    change: new submodule term;

--- home/default.nix
+++ home/default.nix
@@ -8,7 +8,7 @@
   imports = [
     ./ui
     ./filemanager
-    ./zsh.nix
+    ./term
     ./fcitx5
     ./browser
     ./code
@@ -50,39 +50,6 @@
     enable = true;
     userName = "MapoMagpie";
     userEmail = "zsyjk@live.cn";
-  };
-
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "0xProto Nerd Font Mono";
-      size = 16;
-    };
-    shellIntegration = {
-      enableZshIntegration = true;
-      mode = "no-cursor";
-    };
-    # themeFile = "base2tone-suburb-dark";
-    themeFile = "ForestNight";
-    settings = {
-      cursor = "#f0c30f";
-      cursor_text_color = "#f0c30f";
-      cursor_shape = "underline";
-      cursor_trail = 1;
-      cursor_trail_decay = "0.3 0.6";
-      background_opacity = 0.7;
-      symbol_map = "U+3000-U+30ff,U+4e00-U+9fff LXGW WenKai Mono Medium";
-      #     font-family = [
-      #       "0xProto Nerd Font Mono"
-      #       "JetBrainsMono Nerd Font"
-      #       "Noto Sans Mono CJK SC"
-      #     ];
-    };
   };
 
   services.cliphist.enable = true;
