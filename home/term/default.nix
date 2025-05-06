83bd4a5b9dc707a4f522fb808008ac7a088fd898
MapoMagpie <zsyjk@live.cn>
2025-05-06 15:07:39 +0800

    update: use yazi as filechooser;

--- home/term/default.nix
+++ home/term/default.nix
@@ -1,8 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./zsh.nix
   ];
+
+  home.file.".config/xdg-desktop-portal-termfilechooser/config".text = ''
+    [filechooser]
+    cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+    default_dir=$HOME
+    env=TERMCMD=kitty --app-id="kitty.yazi.filechooser"
+  '';
+  home.file.".config/xdg-desktop-portal/portals.conf".text = ''
+    [preferred]
+    org.freedesktop.impl.portal.FileChooser=termfilechooser
+  '';
 
   programs.zoxide = {
     enable = true;
