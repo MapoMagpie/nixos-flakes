3f9c9f00d5a276600dbc1b133e85997144671980
MapoMagpie <zsyjk@live.cn>
2025-05-10 12:39:22 +0800

    add: matugen,quickshell;remove: ironbar,mako;

--- home/ui/default.nix
+++ home/ui/default.nix
@@ -5,7 +5,6 @@
 }:
 {
   imports = [
-    # ./ironbar
     ./niri
     ./fuzzel
   ];
@@ -42,31 +41,6 @@
     };
   };
 
-  # services.mako = {
-  #   enable = true;
-  #   backgroundColor = "#1f1f1fc3";
-  #   width = 500;
-  #   height = 110;
-  #   borderColor = "#f0c30f";
-  #   borderRadius = 0;
-  #   borderSize = 2;
-  #   # icons = false;
-  #   maxIconSize = 64;
-  #   defaultTimeout = 5000;
-  #   # extraConfig = ''
-  #   #   [urgency=low]
-  #   #   border-color=#cccccc
-  #   #   [urgency=normal]
-  #   #   border-color=#f0c30f
-  #   #   [urgency=high]
-  #   #   border-color=#bf616a
-  #   #   default-timeout=50000
-  #   #   [category=mpd]
-  #   #   border-color=#cccccc
-  #   #   default-timeout=2000
-  #   # '';
-  # };
-
   services.hypridle = {
     enable = true;
     settings = {
@@ -88,20 +62,6 @@
       ];
     };
   };
-
-  # services.hyprpaper = {
-  #   enable = true;
-  #   settings = {
-  #     preload = [
-  #       "${config.home.homeDirectory}/nixos/home/images/wallpapers/extend.jpg"
-  #       "${config.home.homeDirectory}/nixos/home/images/wallpapers/paper.jpg"
-  #     ];
-  #     wallpaper = [
-  #       "DP-3,${config.home.homeDirectory}/nixos/home/images/wallpapers/paper.jpg"
-  #       "HDMI-A-1,${config.home.homeDirectory}/nixos/home/images/wallpapers/extend.jpg"
-  #     ];
-  #   };
-  # };
 
   programs.hyprlock = {
     enable = true;
