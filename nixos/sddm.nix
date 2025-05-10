3d015ca2beadb5e663f9d60972019a1cd800c7f0
MapoMagpie <zsyjk@live.cn>
2025-05-10 12:32:29 +0800

    update: sddm theme;

--- nixos/sddm.nix
+++ nixos/sddm.nix
@@ -1,27 +1,19 @@
 { pkgs, ... }:
-let
-  sddm-astronaut = pkgs.callPackage pkgs.sddm-astronaut {
-    embeddedTheme = "hyprland_kath";
-  };
-in
 {
-  environment.systemPackages = [
-    sddm-astronaut
+  nixpkgs.overlays = [
+    (self: super: {
+      sddm-astronaut = super.sddm-astronaut.override {
+        embeddedTheme = "hyprland_kath";
+      };
+    })
   ];
-
-  # nixpkgs.overlays = [
-  #   (self: super: {
-  #     sddm-astronaut = super.sddm-astronaut.override {
-  #       embeddedTheme = "hyprland_kath";
-  #     };
-  #   })
-  # ];
+  environment.systemPackages = [ pkgs.sddm-astronaut ];
 
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     theme = "sddm-astronaut-theme";
     package = pkgs.kdePackages.sddm;
-    extraPackages = [ sddm-astronaut ];
+    extraPackages = [ pkgs.sddm-astronaut ];
   };
 }
