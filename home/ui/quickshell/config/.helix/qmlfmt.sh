5aee1ff2b1e92061ee40b8552995572c8706f623
MapoMagpie <zsyjk@live.cn>
2025-05-10 15:56:55 +0800

    fix: quickshell config helix config;

--- home/ui/quickshell/config/.helix/qmlfmt.sh
+++ home/ui/quickshell/config/.helix/qmlfmt.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 tmpfile=$(mktemp)".qml"
 cat > "$tmpfile"     # 从stdin读取并写入临时文件
-qmlformat "$tmpfile"
+if ! command -v qmlformat >/dev/null 2>&1; then
+  cat $tmpfile
+else
+  qmlformat "$tmpfile"
+fi
 rm "$tmpfile"
