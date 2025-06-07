7041419bc8d49ed22c82857a836c57952b0890e5
MapoMagpie <zsyjk@live.cn>
2025-06-07 19:32:28 +0800

    update: misc;

--- home/default.nix
+++ home/default.nix
@@ -72,12 +72,37 @@
       "image/gif" = [ "swayimg.desktop" ];
       "image/webp" = [ "swayimg.desktop" ];
       "image/svg+xml" = [ "swayimg.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" ];
+      "application/x-extension-html" = [ "firefox.desktop" ];
+      "application/x-extension-shtml" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop" ];
+      "application/x-extension-xht" = [ "firefox.desktop" ];
+      "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop;" ];
+      "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop;" ];
     };
     associations.added = {
       "application/zip" = [ "7z.desktop" ];
       "application/7z" = [ "7z.desktop" ];
       "application/7z-compressed" = [ "7z.desktop" ];
       "application/octet-stream" = [ "7z.desktop" ];
+      "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop;" ];
+      "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop;" ];
+      "x-scheme-handler/http" = [ "firefox.desktop;" ];
+      "x-scheme-handler/https" = [ "firefox.desktop;" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop;" ];
+      "text/html" = [ "firefox.desktop;" ];
+      "application/x-extension-htm" = [ "firefox.desktop;" ];
+      "application/x-extension-html" = [ "firefox.desktop;" ];
+      "application/x-extension-shtml" = [ "firefox.desktop;" ];
+      "application/xhtml+xml" = [ "firefox.desktop;" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop;" ];
+      "application/x-extension-xht" = [ "firefox.desktop;" ];
+
     };
   };
 
