30391938eda2896c410e32bd83171d4d855c8977
MapoMagpie <zsyjk@live.cn>
2025-12-28 15:07:04 +0800

    update: move some programs to system wide;

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,5 @@
 {
-  pkgs,
   host,
-  rimedm,
   ...
 }:
 {
@@ -13,47 +11,10 @@
     ./browser
     ./code
     ./misc
-  ]
-  ++ (if host.hostname == "maponixos" then [ ./game ] else [ ]);
+  ];
 
   home.username = host.username;
   home.homeDirectory = "/home/${host.username}";
-
-  home.packages =
-    with pkgs;
-    [
-      gh
-      git-credential-manager
-      tldr
-      telegram-desktop
-      bat
-      wl-clipboard
-      killall
-      freerdp
-      swayimg
-      rimedm.packages.${pkgs.stdenv.hostPlatform.system}.default
-      gitui
-
-      gnome-keyring
-      slurp
-      libnotify
-      grim
-      wf-recorder
-      dragon-drop
-      showmethekey
-
-    ]
-    ++ (
-      if host.hostname == "maponixos" then
-        [
-          gimp3
-          kdePackages.kdenlive
-          yt-dlp
-          tsukimi
-        ]
-      else
-        [ ]
-    );
 
   programs.git = {
     enable = true;
@@ -74,54 +35,6 @@
       "-max-items"
       "5000"
     ];
-  };
-
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "application/zip" = [ "7z.desktop" ];
-      "application/7z" = [ "7z.desktop" ];
-      "application/7z-compressed" = [ "7z.desktop" ];
-      "application/octet-stream" = [ "7z.desktop" ];
-      "image/png" = [ "swayimg.desktop" ];
-      "image/jpeg" = [ "swayimg.desktop" ];
-      "image/bmp" = [ "swayimg.desktop" ];
-      "image/gif" = [ "swayimg.desktop" ];
-      "image/webp" = [ "swayimg.desktop" ];
-      "image/avif" = [ "swayimg.desktop" ];
-      "image/svg+xml" = [ "swayimg.desktop" ];
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
-      "text/html" = [ "firefox.desktop" ];
-      "application/x-extension-htm" = [ "firefox.desktop" ];
-      "application/x-extension-html" = [ "firefox.desktop" ];
-      "application/x-extension-shtml" = [ "firefox.desktop" ];
-      "application/xhtml+xml" = [ "firefox.desktop" ];
-      "application/x-extension-xhtml" = [ "firefox.desktop" ];
-      "application/x-extension-xht" = [ "firefox.desktop" ];
-      "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop;" ];
-      "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop;" ];
-    };
-    associations.added = {
-      "application/zip" = [ "7z.desktop" ];
-      "application/7z" = [ "7z.desktop" ];
-      "application/7z-compressed" = [ "7z.desktop" ];
-      "application/octet-stream" = [ "7z.desktop" ];
-      "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop;" ];
-      "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop;" ];
-      "x-scheme-handler/http" = [ "firefox.desktop;" ];
-      "x-scheme-handler/https" = [ "firefox.desktop;" ];
-      "x-scheme-handler/chrome" = [ "firefox.desktop;" ];
-      "text/html" = [ "firefox.desktop;" ];
-      "application/x-extension-htm" = [ "firefox.desktop;" ];
-      "application/x-extension-html" = [ "firefox.desktop;" ];
-      "application/x-extension-shtml" = [ "firefox.desktop;" ];
-      "application/xhtml+xml" = [ "firefox.desktop;" ];
-      "application/x-extension-xhtml" = [ "firefox.desktop;" ];
-      "application/x-extension-xht" = [ "firefox.desktop;" ];
-
-    };
   };
 
   # services.mpd.enable = true;
