4f7d248ecec854ee2f409d88e8acf7864046c677
MapoMagpie <zsyjk@live.cn>
2026-02-24 13:05:58 +0800

    add: host slavenixostwo;

--- variables.nix
+++ variables.nix
@@ -1,41 +1,61 @@
 hostname:
 let
-  host =
-    if hostname == "maponixos" then
-      {
-        screen = {
-          w = 2560;
-          h = 1440;
-        };
-        username = "mapomagpie";
-        hostname = "maponixos";
-        userDesc = "Mapo Magpie";
-        userInitPass = "changepassword";
-        hardwareModule = ./hardware/hardware-configuration-maponixos.nix;
-        openssh.authorizedKeys.keys = [ ];
-        git = {
-          userName = "MapoMagpie";
-          userEmail = "zsyjk@live.cn";
-        };
-        upowerEnable = false;
-      }
-    else
-      {
-        screen = {
-          w = 1920;
-          h = 1080;
-        };
-        username = "mapomagpie";
-        hostname = "slavenixos";
-        userDesc = "Mapo Magpie Slave";
-        userInitPass = "changepassword";
-        hardwareModule = ./hardware/hardware-configuration-slavenixos.nix;
-        openssh.authorizedKeys.keys = [ ];
-        git = {
-          userName = "MapoMagpie";
-          userEmail = "zsyjk@live.cn";
-        };
-        upowerEnable = true;
+  host = {
+    "maponixos" = {
+      screen = {
+        w = 2560;
+        h = 1440;
       };
+      username = "mapomagpie";
+      hostname = "maponixos";
+      userDesc = "Mapo Magpie";
+      userInitPass = "changepassword";
+      hardwareModule = ./hardware/hardware-configuration-maponixos.nix;
+      openssh.authorizedKeys.keys = [ ];
+      git = {
+        userName = "MapoMagpie";
+        userEmail = "zsyjk@live.cn";
+      };
+      upowerEnable = false;
+    };
+    "slavenixos" = {
+      screen = {
+        w = 1920;
+        h = 1080;
+      };
+      username = "mapomagpie";
+      hostname = "slavenixos";
+      userDesc = "Mapo Magpie Slave";
+      userInitPass = "changepassword";
+      hardwareModule = ./hardware/hardware-configuration-slavenixos.nix;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHujke7fBecvwbiBDr2FgV6clGXFDCxSeHxvK5Z0tQjv mapomagpie@kamonixos"
+      ];
+      git = {
+        userName = "MapoMagpie";
+        userEmail = "zsyjk@live.cn";
+      };
+      upowerEnable = true;
+    };
+    "slavenixostwo" = {
+      screen = {
+        w = 1920;
+        h = 1080;
+      };
+      username = "mapomagpie";
+      hostname = "slavenixostwo";
+      userDesc = "Mapo Magpie Slave Two";
+      userInitPass = "changepassword";
+      hardwareModule = ./hardware/hardware-configuration-slavenixostwo.nix;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHujke7fBecvwbiBDr2FgV6clGXFDCxSeHxvK5Z0tQjv mapomagpie@kamonixos"
+      ];
+      git = {
+        userName = "MapoMagpie";
+        userEmail = "zsyjk@live.cn";
+      };
+      upowerEnable = true;
+    };
+  };
 in
-host
+host.${hostname}
