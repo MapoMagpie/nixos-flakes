2602493991acb1f6e5d8b073df9ea45dff192a42
MapoMagpie <zsyjk@live.cn>
2026-07-02 10:43:22 +0800

    chore: remove QT_QPA_PLATFORMTHEME system env;

--- nixos/environment.nix
+++ nixos/environment.nix
@@ -4,9 +4,10 @@
     shells = [ pkgs.zsh ];
     variables = {
       EDITOR = "hx";
+      PAGER = "bat";
       TERMINAL = "kitty";
       "QT_QPA_PLATFORM" = "wayland";
-      "QT_QPA_PLATFORMTHEME" = "flatpak";
+      # "QT_QPA_PLATFORMTHEME" = "flatpak";
       "SKIM_DEFAULT_COMMAND" = "fd -d 6 -H -E node_modules -E .git -E '*.lock'";
     };
   };
