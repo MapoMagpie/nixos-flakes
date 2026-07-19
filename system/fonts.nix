7791ead60b90058600c19d258a501c92712090c9
MapoMagpie <zsyjk@live.cn>
2026-07-19 15:52:44 +0800

    update: fonts;

--- system/fonts.nix
+++ system/fonts.nix
@@ -6,12 +6,35 @@
 }:
 {
 
+  # echo -n "serif charset 骨门关复尖 : "; fc-match "serif:charset=9aa8"
+  environment.systemPackages = [ pkgs.fontpreview ];
   fonts = {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [
+          "Roboto Slab"
+          "LXGW WenKai"
+        ];
+        sansSerif = [
+          "Roboto Flex"
+          "LXGW WenKai Screen"
+        ];
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [
+          "0xProto Nerd Font Mono"
+          "LXGW WenKai Mono"
+        ];
+      };
+    };
     packages =
       with pkgs;
       [
+        roboto-slab # bold serif
+        roboto-flex # sans serif
+        roboto-mono
+        roboto-serif
         nerd-fonts._0xproto
-        nerd-fonts._3270
         noto-fonts-color-emoji
         lxgw-wenkai
         lxgw-neoxihei
@@ -19,6 +42,8 @@
         lxgw-fusionkai
         lxgw-wenkai-screen
         sarasa-gothic
+        dejavu_fonts
+        unifont
         externalFonts.default
       ]
       ++ (
@@ -31,7 +56,7 @@
       )
 
     ;
-    enableDefaultPackages = true;
+    enableDefaultPackages = false;
     fontDir.enable = true;
   };
 
