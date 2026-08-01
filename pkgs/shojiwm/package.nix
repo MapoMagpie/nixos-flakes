dede02ef4ecf1894b245b33c57d6c503b5bb55cb
MapoMagpie <zsyjk@live.cn>
2026-08-01 08:20:06 +0800

    update: shojiwm package;

--- pkgs/shojiwm/package.nix
+++ pkgs/shojiwm/package.nix
@@ -13,13 +13,13 @@
 # at the pinned revision. To update, bump `rev` / `hash` via ./update.sh.
 
 let
-  rev = "d9802e3ef34a4477a92e87915c75627b5a78babc";
+  rev = "268c7970912e85dca11e64bbecde8140d8431b26";
 
   src = fetchFromGitHub {
     owner = "bea4dev";
     repo = "ShojiWM";
     inherit rev;
-    hash = "sha256-aEwwQ9utsaCKoPC1i4P9RJk5NWQk69xPAYWfuFnJ1qg=";
+    hash = "sha256-2hTcsBMQW5kKVnfsC4xsobKlyHFqL3is+d9RpaxO6E0=";
   };
 in
 callPackage (src + "/nix/package.nix") {
